9d2437fb16f79ebe70c3c16da0454aef55b0132a
RajendranEshwaran <rajendranmca2010@gmail.com>
2019-12-22 11:46:29 -0500

    Showing day weather data in tableview after retrieve from the service

--- DarkSky2/ViewController.swift
+++ DarkSky2/ViewController.swift
@@ -8,25 +8,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UITableViewDelegate , UITableViewDataSource{
+    
 
+    @IBOutlet weak var dayWeatherTableView: UITableView!
     var weathermodel = [WeatherModel]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        dayWeatherTableView.delegate = self
+        
         WeatherFetch.weatherFethData(withLocation: "37.8267,-122.4233")  {(result:[WeatherModel]) in
         
             for result in result{
-                //print(result.tempra)
                 
                 self.weathermodel.append(result)
-                print(self.weathermodel)
+                DispatchQueue.main.async {
+                    self.dayWeatherTableView.reloadData()
+                }
             }
         }
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return weathermodel.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DayWeatherCell", for: indexPath)
+        let data = weathermodel[indexPath.row]
+        
+        cell.textLabel?.text = " Day:  \(String (data.tempra))"
+        
+        return cell
+    }
+    
 
 }
 
